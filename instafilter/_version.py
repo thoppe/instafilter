2df916626b58538133b9e880f9538e14e4269cb5
thoppe <travis.hoppe@gmail.com>
2022-12-05 00:24:07 -0500

    Fix CPU bug from #2

--- instafilter/_version.py
+++ instafilter/_version.py
@@ -1,6 +1,7 @@
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 """
+Version: 0.3.2: Fix cpu bug.
 Version: 0.3.1: Add opencv-python into the setup for loading images.
 Version: 0.3.0: Option for RGB images.
 Version: 0.2.0: Add batch_size as an option to handle larger images.
